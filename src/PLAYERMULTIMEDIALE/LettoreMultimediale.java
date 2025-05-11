09c3ecef2bd470418a61c62afb5c9cc1823f716d
pandino126 <re.luca95@gmail.com>
2025-05-11 16:57:07 +0200

    finito, spero

--- src/PLAYERMULTIMEDIALE/LettoreMultimediale.java
+++ src/PLAYERMULTIMEDIALE/LettoreMultimediale.java
@@ -1,5 +1,6 @@
 package PLAYERMULTIMEDIALE;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class LettoreMultimediale {
@@ -22,11 +23,11 @@ public class LettoreMultimediale {
         Video videoclip = new Video("Ngueee", 3, 4, 2);
         videoclip.play();
         //ABBASSO IL VOLUME E LA LUMINOSITA DI 1
-      videoclip.abbassaVolume();
-      videoclip.abbassaLuminosita();
-      videoclip.play();
+        videoclip.abbassaVolume();
+        videoclip.abbassaLuminosita();
+        videoclip.play();
         //---------------------IMMAGINE-------------------------------
-        Image foto= new Image("Programmatore che piange", 2);
+        Image foto = new Image("Programmatore che piange", 2);
         foto.show();
         //AUMENTO LA LUMINOSITA DI 1
         foto.aumentaLuminosita();
@@ -48,5 +49,67 @@ public class LettoreMultimediale {
             default:
                 System.out.println("Scelta non valida");
         }
+        //----------------ARRAY--------------------
+        ElementoMultimediale[] elementi = new ElementoMultimediale[5];
 
-    }}
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Inserisci tipo elemento (1: Immagine, 2: Video, 3: Audio):");
+                    int sceltaTipo = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.print("Titolo: ");
+                    String titolo = scanner.nextLine();
+
+                    switch (sceltaTipo) {
+                        case 1:
+                            System.out.print("Luminosità: ");
+                            int lum = scanner.nextInt();
+                            elementi[i] = new Image(titolo, lum);
+                            break;
+                        case 2:
+                            System.out.print("Durata: ");
+                            int durataVideo = scanner.nextInt();
+                            System.out.print("Volume: ");
+                            int volVideo = scanner.nextInt();
+                            System.out.print("Luminosità: ");
+                            int lumVideo = scanner.nextInt();
+                            elementi[i] = new Video(titolo, durataVideo, volVideo, lumVideo);
+                            break;
+                        case 3:
+                            System.out.print("Durata: ");
+                            int durataAudio = scanner.nextInt();
+                            System.out.print("Volume: ");
+                            int volAudio = scanner.nextInt();
+                            elementi[i] = new RegistrazioneAudio(titolo, durataAudio, volAudio);
+                            break;
+                        default:
+                            System.out.println("Scelta non valida. Riprova.");
+                            i--;
+                    }
+                }
+
+                int scelta;
+                do {
+                    System.out.println("Scegli un elemento da 1 a 5 da eseguire, oppure 0 per uscire:");
+                    scelta = scanner.nextInt();
+
+                    if (scelta >= 1 && scelta <= 5) {
+                        ElementoMultimediale elemento = elementi[scelta - 1];
+
+                        if (elemento instanceof Video) {
+                            ((Video) elemento).play();
+                        } else if (elemento instanceof RegistrazioneAudio) {
+                            ((RegistrazioneAudio) elemento).play();
+                        } else if (elemento instanceof Image) {
+                            ((Image) elemento).show();
+                        }
+
+                    } else if (scelta != 0) {
+                        System.out.println("Scelta non valida.");
+                    }
+
+                } while (scelta != 0);
+
+                System.out.println("Fine esecuzione. Saluta Andonioooo");
+            }
+        }
